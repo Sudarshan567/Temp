397d8bf8e8eb2480ee9f4d5e59f49ba5705f59e7
Sudarshan K <133846499+Sudarshan567@users.noreply.github.com>
2024-04-14 21:59:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function menuclick()
+{
+    var menu=document.getElementById("mobileMenu");
+    if(menu.classList.contains('hidden'))
+    {
+        menu.classList.remove('hidden');
+    }
+    else{
+        menu.classList.add('hidden');
+    }
+}
